5e956d50faaed54077a7f8b7579e4e0420b9eb25
takemura-ayumi <atakemura@nextreservation.jp>
2025-07-15 14:49:55 +0900

    0715

--- c/yamaguti-denki/job_electrician/index.php
+++ c/yamaguti-denki/job_electrician/index.php
@@ -86,6 +86,9 @@ $og_image = 'https://works.mekulo.jp/ogp-image.png';
         <h1 class="catch"><?= $catch_copy ?></h1>
         <p class="fv-tag"><?= $name ?></p>
     </hgroup>
+		<a href="https://youtu.be/BS2_BUE3yB8?feature=shared" class="video-linkk" target="testvideo">
+			<img src="<?= $highschool_base ?>with-video.png" alt="">
+		</a>
     <?php if (count($occupation) >= 2): // 配列数が2以上か確認し、無い場合は非表示にする ?>
         <a href="<?= $company_base . $next_job['url'] ?>/" class="btn">次の職種を見る</a>
     <?php endif; ?>
